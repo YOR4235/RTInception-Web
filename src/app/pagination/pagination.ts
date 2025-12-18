86e739ad04f795072688aff12e6e60dacee4166d
Rathnam <2430724@cognizant.com>
2025-12-18 16:19:20 +0530

    pagination changes

--- src/app/pagination/pagination.ts
+++ src/app/pagination/pagination.ts
@@ -11,11 +11,35 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 })
 export class Pagination {
 
-  @Input() totalPages: number = 10;
+  @Input() totalPages: number = 11;
   @Input() currentPage: number = 1;
-  pageSize: number = 10;
+  pageSize: number = 11;
 
   constructor(private ideaEvents: IdeaEventsService) {}
+  
+get paginationItems(): (number | 'ellipsis')[] {
+    const items: (number | 'ellipsis')[] = [];
+
+    // If small totals, just show all pages (no ellipsis)
+    if (this.totalPages <= 6) {
+      for (let i = 1; i <= this.totalPages; i++) items.push(i);
+      return items;
+    }
+
+  // Always show pages 1..5
+    for (let i = 1; i <= 5; i++) items.push(i);
+
+    // If there are pages between 5 and last, show a single ellipsis
+    if (this.totalPages > 6) {
+      items.push('ellipsis');
+    }
+
+    // Always show last page
+    items.push(this.totalPages);
+
+    return items;
+  }
+
 
   prev() {
     if (this.currentPage > 1) {
