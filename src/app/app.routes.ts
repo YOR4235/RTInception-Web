8cff1c81097256818bce2ebd68763ea9e70ed4dd
karthik <karthikperisetti@gmail.com>
2025-12-19 12:43:55 +0530

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { IdeaListParent } from './idea-list-parent/idea-list-parent';
 import { Addidea } from './addidea/addidea';
+import { Myideas } from './ideas/myideas/myideas';
+import { IdeaDashboard } from './ideas/idea-dashboard/idea-dashboard';
 
 export const routes: Routes = [
-  { path: '', component: IdeaListParent },
+  { path: '', component: IdeaDashboard },
   { path: 'addidea', component: Addidea },
+  { path: 'myideas', component: Myideas}
 ];
--- src/app/statustabs/statustabs.ts
+++ src/app/statustabs/statustabs.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Idea } from '../models/idea';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatDividerModule } from '@angular/material/divider';
@@ -9,7 +9,7 @@ import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-statustabs',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatTabsModule,MatDividerModule],
+  imports: [CommonModule, MatIconModule, MatTabsModule,MatDividerModule, RouterModule],
   templateUrl: './statustabs.html',
   styleUrl: './statustabs.scss',
 })
